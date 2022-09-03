3d19e0262d78c71314858d702fbffe5d68381d77
Pedro Ramalho <pedro_hrr@hotmail.com>
2022-09-02 21:11:12 -0300

    add new things

--- src/main/java/Vendas/application.java
+++ src/main/java/Vendas/application.java
@@ -1,13 +1,29 @@
 package Vendas;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class application {
+
+    @Autowired
+    // injetando o valor do bean nesse atributo
+    @Qualifier("applicationName")
+    public String applicationName;
+
+    @GetMapping("/hello")
+    public String helloWorld()
+    {
+        return applicationName;
+    }
 
     public static void main(String[] args) {
 
-        SpringApplication.run(application.class, args);
+                        SpringApplication.run(application.class,args);
     }
 }
