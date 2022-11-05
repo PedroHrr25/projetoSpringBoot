c94a72dbd0764c2df9296076c16eccf525884448
Pedro Ramalho <pedro_hrr@hotmail.com>
2022-11-05 01:11:26 -0300

    algumas atualizacoes

--- src/main/java/Vendas/application.java
+++ src/main/java/Vendas/application.java
@@ -1,16 +1,36 @@
 package Vendas;
 
+import Vendas.domain.entity.Cliente;
+import Vendas.domain.repositorio.Clientes;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 @RestController
 public class application {
+
+  /* comando de teste  @Bean
+    public CommandLineRunner init (@Autowired Clientes clientes){
+        return args -> {
+            Cliente cliente = new Cliente();
+            cliente.setNome("Pedro");
+            clientes.salvar(cliente);
+        };
+    }
+
+   */
+
+
+
+
+
     public static void main(String[] args) {
               SpringApplication.run(application.class,args);
     }
--- src/main/java/Vendas/domain/entity/Cliente.java
+++ src/main/java/Vendas/domain/entity/Cliente.java
@@ -4,6 +4,26 @@ public class Cliente {
     private Integer id;
     private String nome;
 
+    public Cliente(Integer id, String nome) {
+        this.id = id;
+        this.nome = nome;
+    }
+
+    @Override
+    public String toString() {
+        return "Cliente{" +
+                "id=" + id +
+                ", nome='" + nome + '\'' +
+                '}';
+    }
+
+    public Cliente() {
+    }
+
+    public Cliente(String nome) {
+        this.nome = nome;
+    }
+
     public Integer getId() {
         return id;
     }
