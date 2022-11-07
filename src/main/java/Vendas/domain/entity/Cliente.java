9335eb1042872f69342c171807904eb06084a79f
Pedro Ramalho <pedro_hrr@hotmail.com>
2022-11-07 14:33:00 -0300

    trabalhando com jdbctemplate e transactional

--- src/main/java/Vendas/domain/entity/Cliente.java
+++ src/main/java/Vendas/domain/entity/Cliente.java
@@ -1,20 +1,28 @@
 package Vendas.domain.entity;
 
+
+import net.bytebuddy.dynamic.loading.ClassReloadingStrategy;
+import net.bytebuddy.dynamic.loading.InjectionClassLoader;
+import org.hibernate.boot.registry.selector.spi.StrategyCreator;
+
+import javax.persistence.*;
+
+@Entity
+// @Table = para utlizar com um nome de tabela diferente ou schema
+@Table (name = "cliente")
 public class Cliente {
+
+    // a função de primary key e auto imcremento
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "ID")
     private Integer id;
+    @Column(name = "nome", length = 100)
     private String nome;
 
     public Cliente(Integer id, String nome) {
         this.id = id;
         this.nome = nome;
-    }
-
-    @Override
-    public String toString() {
-        return "Cliente{" +
-                "id=" + id +
-                ", nome='" + nome + '\'' +
-                '}';
     }
 
     public Cliente() {
@@ -40,3 +48,5 @@ public class Cliente {
         this.nome = nome;
     }
 }
+
+  
--- src/main/java/Vendas/domain/repositorio/Clientes.java
+++ src/main/java/Vendas/domain/repositorio/Clientes.java
@@ -6,58 +6,80 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.core.RowMapper;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
+import javax.persistence.EntityManager;
+import javax.persistence.TypedQuery;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.List;
 
 @Repository
 public class Clientes {
-
-    private static String INSERT = "insert into cliente (nome values (?) ";
-    private static String SELECT_ALL = "select * from cliente ";
-    private static String UPDATE = "update cliente set nome = ? where id = ?";
-    private static String DELETE = "delete from cliente where id = ? ";
+// só para recapitular as partes de jdbc
+    // porem, ira ser iniciado com JPA
+//
+//   private static String INSERT = "insert into cliente (nome values (?) ";
+//    private static String SELECT_ALL = "select * from cliente ";
+//    private static String UPDATE = "update cliente set nome = ? where id = ?";
+//    private static String DELETE = "delete from cliente where id = ? ";
 
     @Autowired
     private JdbcTemplate jdbcTemplate;
+    @Autowired
+    private EntityManager entityManager;
 
-
+    @Transactional
     public Cliente salvar(Cliente cliente) {
-        jdbcTemplate.update(INSERT, new Object[]{cliente.getNome()});
+        entityManager.persist(cliente);
+        //      jdbcTemplate.update(INSERT, new Object[]{cliente.getNome()});
         return cliente;
     }
 
+    @Transactional
     public Cliente update(Cliente cliente) {
-        jdbcTemplate.update(UPDATE, new Object[]{cliente.getNome(), cliente.getId()});
+        entityManager.merge(cliente);
         return cliente;
     }
 
+    @Transactional
+    public void deletar(Cliente cliente) {
+        if (!entityManager.contains(cliente)){
+            cliente = entityManager.merge(cliente);
+        }
+        entityManager.remove(cliente);
+    }
+
+    @Transactional
     public void deletar(Integer id) {
-        jdbcTemplate.update(DELETE, new Object[]{id});
+        Cliente cliente = entityManager.find(Cliente.class, id);
+        deletar(id);
     }
 
-
+    @Transactional
     public List<Cliente> obterTodos() {
-        return jdbcTemplate.query(SELECT_ALL, obterClienteMapper());
+        return entityManager.createQuery("from Cliente", Cliente.class).getResultList();
     }
 
+    //opmitizar pesquisa o readonly
+    @Transactional(readOnly = true)
     public List<Cliente> buscarPorNome(String nome) {
-        return jdbcTemplate.query(SELECT_ALL.concat(" where nome like ? "),
-                new Object[]{"%" + nome + "%"},
-                obterClienteMapper());
+        String jpql = " select c from Cliente c where c.nome like :nome";
+        TypedQuery<Cliente> query = entityManager.createQuery(jpql, Cliente.class);
+        query.setParameter("nome", "%" + nome + "%");
+        return query.getResultList();
     }
 
 
-    private RowMapper<Cliente> obterClienteMapper() {
-        return new RowMapper<Cliente>() {
-            // mapear as colunas para a entidade clientes e retorna "arrumado"
-            @Override
-            public Cliente mapRow(ResultSet resultSet, int i) throws SQLException {
-                String nome = resultSet.getString("nome");
-                Integer id = resultSet.getInt("id");
-                return new Cliente(id, nome);
-            }
-        };
-    }
+//    private RowMapper<Cliente> obterClienteMapper() {
+//        return new RowMapper<Cliente>() {
+//            // mapear as colunas para a entidade clientes e retorna "arrumado"
+//            @Override
+//            public Cliente mapRow(ResultSet resultSet, int i) throws SQLException {
+//                String nome = resultSet.getString("nome");
+//                Integer id = resultSet.getInt("id");
+//                return new Cliente(id, nome);
+//            }
+//        };
 }
+
