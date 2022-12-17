9760cebe2b1fc787ec9a4467c1eb20948c13a99c
Pedro Ramalho <pedro_hrr@hotmail.com>
2022-12-17 03:57:04 -0300

    terminando o spring security e validação para o banco auth.

--- src/main/java/Vendas/config/SecurityConfig.java
+++ src/main/java/Vendas/config/SecurityConfig.java
@@ -1,6 +1,9 @@
 package Vendas.config;
 
+import Vendas.service.impl.UsuarioServiceImpl;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -11,7 +14,9 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
+@Autowired
 
+private UsuarioServiceImpl usuarioService;
 
     // encoder de senha , gerando hash para cada senha gerada
     @Bean
@@ -19,13 +24,13 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
         return new BCryptPasswordEncoder();
     }
 
+
+
+    //mudança do cadastro para ser via banco e cadastrar no sistema
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-        auth.inMemoryAuthentication()
-                .passwordEncoder(passwordEncoder())
-                .withUser("fularo")
-                .password(passwordEncoder().encode("123"))
-                .roles("USER");
+        auth.userDetailsService(usuarioService)
+                .passwordEncoder(passwordEncoder());
     }
 
     @Override
@@ -33,9 +38,16 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
         http.csrf().disable()
                 .authorizeRequests()
                 .antMatchers("/api/clientes/**")
-                .authenticated()
+                .hasAnyRole("ADMIN","USER")
+                .antMatchers("/api/produtos/**")
+                .hasRole("ADMIN")
+                .antMatchers("api/pedidos/**")
+                .hasAnyRole("ADMIN","USER")
+                .antMatchers(HttpMethod.POST, "/api/usuarios/**")
+                .permitAll()
+                .anyRequest().authenticated()
                 .and()
-                .formLogin();
+                .httpBasic();
     }
 
 //    /**
---
+++ src/main/java/Vendas/domain/repository/UsuarioRepository.java
@@ -0,0 +1,14 @@
+package Vendas.domain.repository;
+
+import Vendas.domain.entity.Usuario;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface UsuarioRepository extends JpaRepository<Usuario , Integer> {
+
+
+    Optional<Usuario> findByLogin(String login);
+
+
+}
